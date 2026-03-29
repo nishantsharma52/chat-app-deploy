e554fe99e12e7ff128f2a91cf1fd39dcb038749c
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-29 08:08:17 +0530

    add database

---
+++ backend/config/database.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import dns from "node:dns"; // 1. Yeh naya import add karo
+
+// 2. Node.js ko Google ka DNS use karne ke liye force karo (SRV error bypass)
+dns.setServers(["8.8.8.8", "8.8.4.4"]); 
+
+dotenv.config();
+
+const databaseConnection = async () => {
+    // 3. Optional: Chai aur Code series mein Hitesh sir DB name alag se lagwate hain
+    // Agar tumhare MONGO_URI ke end mein DB ka naam nahi hai, toh yahan add kar lena
+   await mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log("MongoDB connected successfully");
+    }).catch((error) => {
+        console.log("Connection Error:", error);
+    });
+};
+
+export default databaseConnection;
