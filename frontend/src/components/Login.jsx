d445910e92b1398a40e7f541608b7d0debef96da
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-02 16:21:03 +0530

    add login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 
 const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+
+  })
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    setUser({
+      username: "",
+      password: "",
+
+    })
+
+  }
   return (
     <div className='min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-white/10 backdrop-blur-md border border-white/20'>
         <h1 className='text-3xl font-bold text-center'>Login</h1>
-        <form action="">
+        <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className='label p-2'>
               <span className='text-base label-text '>Username </span>
             </label>
-            <input className='w-full input input-borderd h-10' type="text" placeholder='username' />
+            <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} className='w-full input input-borderd h-10' type="text" placeholder='username' />
           </div>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text '>Password </span>
             </label>
-            <input className='w-full input input-borderd h-10' type="password" placeholder='password' />
+            <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} className='w-full input input-borderd h-10' type="password" placeholder='password' />
           </div>
-         
-          
+
+
           <p className=' text-center my-2 '> Don't have an account?  <Link to="/register"> signup</Link> </p>
           <div>
-            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Login</button>
+            <button type='submit' className='btn btn-block btn-sm mt-2 border border-slate-700'>Login</button>
           </div>
 
         </form>
