e588a05e498e01b9b9847feaacbcc9f9b48db6ca
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-05 13:57:24 +0530

    updated code

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
 
-const Message = () => {
+const Message = ({message}) => {
+  const scroll = useRef()
+  const {authUser, selectedUser} = useSelector(store=>store.user)
+
+  useEffect(()=>{
+    scroll.current?.scrollIntoView({behavior:"smooth"})
+  })
   return (
-   <div className="chat chat-start">
+      <div ref={scroll} className={`chat ${message?.senderId?.toString() === authUser?._id?.toString() ? "chat-end" : "chat-start"}`}>
   <div className="chat-image avatar">
     <div className="w-10 rounded-full">
       <img
         alt="Tailwind CSS chat bubble component"
-        src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+       src={message?.senderId === authUser?._id ? authUser?.profilePhoto : selectedUser?.profilePhoto}
       />
     </div>
   </div>
   <div className="chat-header">
     <time className="text-xs opacity-50 ">12:45</time>
   </div>
-  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-bubble">{message?.message}</div>
  
 </div>
   )
