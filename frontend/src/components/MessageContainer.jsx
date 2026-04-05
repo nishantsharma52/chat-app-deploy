7b314cb8e4e033220486d94d71f31e3930c6f9cb
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-05 13:57:28 +0530

    updated code

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SendInput from './SendInput'
 import Messages from './Messages'
+import { useDispatch, useSelector } from 'react-redux'
+import { setSelectedUser } from "../redux/userSlice"
 
 const MessageContainer = () => {
+    const { selectedUser, authUser } = useSelector(store => store.user)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        return () => dispatch(setSelectedUser(null))
+    }, [])
     return (
-        <div className=' md:min-w-[550px] flex-col flex '>
-                <div>
-                    <div className='flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2'>
-                        <div className='avatar online'>
-                            <div className='w-12 rounded-full'>
-                                <img src="https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg" alt="user-profile" />
+        <>
+            {
+                selectedUser !== null ? (
+                    <div className=' md:min-w-[550px] flex-col flex '>
+                        <div>
+                            <div className='flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2'>
+                                <div className='avatar online'>
+                                    <div className='w-12 rounded-full'>
+                                        <img src={selectedUser?.profilePhoto} alt="user-profile" />
+                                    </div>
+                                </div>
+                                <div className='flex flex-col flex-1'>
+                                    <div className='flex justify-between  gap-2 '>
+                                        <p>{selectedUser?.fullName}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className='flex flex-col flex-1'>
-                            <div className='flex justify-between  gap-2 '>
-                                <p>Nishnat Sharma</p>
-                            </div>
-                        </div>
+                        <Messages />
+                        <SendInput />
                     </div>
 
-                </div>
-                <Messages/>
-                <SendInput/>
-        </div>
+                ) : (
+                    <div className=' md:min-w-[550px] flex-col flex justify-center items-center '>
+                        <h1 className='text-4xl text-white font-bold'>Hi,{authUser?.fullName}</h1>
+                        <h1 className='text-2xl text-white'>Let's Start Conversation</h1>
+                    </div>
+                )
+            }
+        </>
+
     )
 }
 
