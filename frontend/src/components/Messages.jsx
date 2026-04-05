f9a049ecfdd8d3ad4ed9abc4686feb6f8758f97e
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-05 13:57:34 +0530

    updated code

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Message from './Message'
+import useGetMessages from '../hooks/useGetMessages'
+import { useSelector } from 'react-redux'
 
 const Messages = () => {
+  useGetMessages()
+  const {messages} = useSelector(store=>store.message)
+  console.log(messages);
+  
+  if(!messages) return;
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-       
+      {
+        messages && messages?.map((message) => {
+          return (
+            <Message key={message._id} message={message} />
+          )
+        })
+      }
 
     </div>
   )
