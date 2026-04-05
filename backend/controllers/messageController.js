a97d6bb5e383d5722cbd192323a2a712298f08f7
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-05 13:56:58 +0530

    updated code

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -25,7 +25,7 @@ export const sendMessage = async (req, res) => {
         await gotConversation.save()
 
         return res.status(201).json({
-            message:"Message send successfully.."
+           newMessage
         })
 
      //   socket IO
