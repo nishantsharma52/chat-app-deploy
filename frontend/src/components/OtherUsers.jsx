b87fc9f0aeca6dc7565872e4dac2760e38c06030
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-15 07:34:56 +0530

    improve responsiveness

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -3,22 +3,36 @@ import OtherUser from './OtherUser'
 import useGetOtherUsers from '../hooks/useGetOtherUsers'
 import { useSelector } from 'react-redux'
 
-const OtherUsers = () => {
-   // my custom hook
+// 1. Yahan 'search' prop receive karein jo Sidebar se aa raha hai
+const OtherUsers = ({ search }) => { 
+  // my custom hook
   useGetOtherUsers()
-const {otherUsers} = useSelector(store=>store.user)
-if (!otherUsers) return null;
+  const { otherUsers } = useSelector(store => store.user)
+
+  if (!otherUsers) return null;
+
+  // 2. Redux wale 'otherUsers' ko filter karein
+  // Agar 'search' khali ("") hai, toh 'includes' saare users return kar dega
+  // Agar kuch type kiya hai, toh sirf matching users return honge
+  const displayUsers = otherUsers.filter((user) => 
+     user.fullName.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className='overflow-auto flex-1'>
+      {/* 3. 'otherUsers' ki jagah 'displayUsers' par map chalayein */}
       {
-        otherUsers?.map((user) =>{
-          return(
-              <OtherUser key={user._id} user={user}/>
-          )
-        })
+        displayUsers.length > 0 ? (
+          displayUsers.map((user) => {
+            return (
+              <OtherUser key={user._id} user={user} />
+            )
+          })
+        ) : (
+          // Agar search karne par koi user na mile toh ye dikhayega
+          <p className="text-center text-slate-400 mt-4">No user found</p>
+        )
       }
-    
-    
     </div>
   )
 }
